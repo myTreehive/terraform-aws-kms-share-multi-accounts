534735b0211f9a3f1fa339d3818a7b4757c8a811
Alex Rygalov <14346955+alexrygalov@users.noreply.github.com>
2022-07-04 10:09:51 +0300

    TR-1987 Refactor kms key module for latest terraform, cleanup

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "key_arn" {
-  value = aws_kms_key.devops.arn
+  value = aws_kms_key.this.arn
 }
+
 output "key_id" {
-  value = aws_kms_key.devops.id
+  value = aws_kms_key.this.id
 }
+
 output "key_alias_id" {
   value = aws_kms_alias.alias.arn
 }
+
 output "key_alias_arn" {
   value = aws_kms_alias.alias.target_key_arn
 }
-output "policy" {
-  value = data.template_file.pqw.rendered
+
+output "key_policy" {
+  value = data.aws_iam_policy_document.kms-key-policy.json
 }
--- variables.tf
+++ variables.tf
@@ -10,20 +10,29 @@ variable "tags" {
     Owner = "devops"
   }
 }
+
 variable "key_name" {}
+
+variable "key_deletion_window" {
+  description = "The waiting period, specified in number of days. After the waiting period ends, AWS KMS deletes the KMS key. If you specify a value, it must be between 7 and 30, inclusive. If you do not specify a value, it defaults to 30"
+  type        = number
+  default     = 7
+}
+
 variable "user_policy" {
   default = ""
 }
 
 variable "dest_account_ids" {}
+
 variable "src_account_ids" {}
+
 variable "dest_iam_roles" {
   type    = list(string)
   default = []
 }
+
 variable "src_iam_roles" {
   type    = list(string)
   default = []
 }
-
-
